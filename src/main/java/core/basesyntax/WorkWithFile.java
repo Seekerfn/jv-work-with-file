57362c02dddfc72144651a79948bc770e87fe684
Zarytskyi Oleksandr <pfantom180@gmail.com>
2026-05-26 21:23:40 +0300

    task is done!

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,47 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+
+    private StringBuilder stringBuilder = new StringBuilder();
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        int buy = 0;
+        int supply = 0;
+        File file = new File(fromFileName);
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file)); BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName, true))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] array = line.split(",");
+                for (int i = 0; i < array.length; i++) {
+                    if (array[i].equals("buy")) {
+                        buy += Integer.parseInt(array[1]);
+                    } else if (array[i].equals("supply")) {
+                        supply += Integer.parseInt(array[1]);
+                    }
+
+                }
+
+            }
+            String string = stringBuilder.toString();
+
+            int result = buy - supply;
+
+            bufferedWriter.write("supply" + "," + supply + "\n" + "buy" + "," + buy + "\n" + "result" + "," + result);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read the file");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        WorkWithFile file = new WorkWithFile();
+        file.getStatistic("banana.csv", "test1.csv");
     }
 }
